2236adfd0e2c274c9ec0cd895b6b8fbace664149
Morgan <morganlote@gmail.com>
2023-12-06 15:23:07 +0000

    added new file that git points to

---
+++ index.js
@@ -0,0 +1,11 @@
+//file that npm points to, just imports everything and exports it again
+import {render} from "./render.js"
+import {getRenderedColourCodes} from "./getRenderedColourCodes.js"
+import {renderText} from "./renderText.js"
+import {getRenderedText} from "./getRenderedText.js"
+import {colourCodes} from "./colourCodes.js"
+export {render}
+export {getRenderedColourCodes}
+export {renderText}
+export {getRenderedText} 
+export {colourCodes}
