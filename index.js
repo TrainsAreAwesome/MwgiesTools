a2c871ea7b79f932debd04a9a0609ddba8fd93c8
Morgan <morganlote@gmail.com>
2023-12-06 15:27:50 +0000

    trying to work out how npm works

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //file that npm points to, just imports everything and exports it again
+export {mwgiesTools}
 import {render} from "./render.js"
-import {getRenderedColourCodes} from "./getRenderedColourCodes.js"
-import {renderText} from "./renderText.js"
-import {getRenderedText} from "./getRenderedText.js"
-import {colourCodes} from "./colourCodes.js"
-export {render}
-export {getRenderedColourCodes}
-export {renderText}
-export {getRenderedText} 
-export {colourCodes}
+import {getRenderedColourCodes, getRenderedColourCodes} from "./getRenderedColourCodes.js"
+import {renderText, renderText} from "./renderText.js"
+import {getRenderedText, getRenderedText} from "./getRenderedText.js"
+import {colourCodes, colourCodes} from "./colourCodes.js"
+
+let mwgiesTools = () => {
+    let render = render()
+    let getRenderedColourCodes = getRenderedColourCodes()
+    let renderText = renderText()
+    let getRenderedText = getRenderedText()
+    let colourCodes = colourCodes()
+}
