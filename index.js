069d6a9601517337acc82881f5e539b2e1410601
Morgan <morganlote@gmail.com>
2023-12-09 14:02:26 +0000

    another attempt

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import chalk from "chalk"
+import chalk from "chalk" //exporting all functions of programme so people can actaully use it from npm
 import {render} from "./render.js"
 import {getRenderedColourCodes} from "./getRenderedColourCodes.js"
 import {renderText} from "./renderText.js"
 import { getRenderedText } from "./getRenderedText.js"
+export {render}
+export {getRenderedColourCodes}
+export {renderText}
+export {getRenderedText}
